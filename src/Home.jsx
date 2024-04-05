c4bc75ee4889c69d93ae2bfb1cfd8974d4119529
liamdtodd <91576595+liamdtodd@users.noreply.github.com>
2024-04-04 22:54:51 -0700

    css import, layout

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,21 +1,39 @@
+import { useState } from "react"
 import { css } from "@emotion/react"
 
-export default function Home() {
-    const divStyles = css`
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
+import PortfolioPage from "./components/PortfolioPage";
+import Resume from "./components/Resume";
+import Languages from "./components/Languages";
+import Projects from "./components/Projects";
+import About from "./components/About";
 
-        h1, p {
-            color: white;
-        }
-    `
+export default function Home() {
 
     return (
-        <div css={divStyles}>
-            <h1>In Development!</h1>
-            <p>Come Back Soon!</p>
-        </div>
+        <>
+            <div className="header-container">
+                <h1>Liam Todd's Portfolio</h1>
+
+                <div className="header-contact-container">
+                    <h2>503-480-5155</h2>
+                    <h2>liamd.todd5@gmail.com</h2>
+                </div>
+            </div>
+
+            <div className="portfolio-page-container">
+                <PortfolioPage />
+            </div>
+
+            <div className="projects-container">
+                <h1>Projects</h1>
+                <div className="row-container">
+                    <Projects />
+                </div>
+            </div>
+
+            <div className="resume-page-container">
+                <Resume />
+            </div>
+        </>
     )
 }
