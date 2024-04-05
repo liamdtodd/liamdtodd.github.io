ce04a49d153726218f706dd854f8a27c6d777f5a
liamdtodd <91576595+liamdtodd@users.noreply.github.com>
2024-04-04 22:57:02 -0700

    incomplete

---
+++ src/components/Resume.jsx
@@ -0,0 +1,17 @@
+export default function Resume() {
+    return (
+        <>
+            <div className="capturedsun-card">
+                <h1>Captured Sun</h1>
+                <h2>SWE Intern</h2>
+                <h3>September 2023 - June 2024</h3>
+                <p>Key tasks in Parchment
+                    <ul>Worked with a team of developers to start building the native Windows desktop app of Parchment</ul>
+                    <ul>Collaborated with senior developers to design and implement front-end and back-end features for Parchment</ul>
+                    <ul>Scheduled meetings with other interns to coordinate smaller sub-tasks</ul>
+                    <ul>Assisted with debugging to improve performance</ul>
+                </p>
+            </div>
+        </>
+    )
+}
