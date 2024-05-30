10470704209a234c0f8055d2892173f897910a04
liamdtodd <91576595+liamdtodd@users.noreply.github.com>
2024-05-30 12:45:39 -0700

    new home page layout

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,9 +1,7 @@
 import { useState } from "react"
 import { css } from "@emotion/react"
 
-import PortfolioPage from "./components/PortfolioPage";
 import Resume from "./components/Resume";
-import Languages from "./components/Languages";
 import Projects from "./components/Projects";
 import About from "./components/About";
 
@@ -11,29 +9,21 @@ export default function Home() {
 
     return (
         <>
-            <div className="header-container">
+            <header className="header-container">
                 <h1>Liam Todd's Portfolio</h1>
 
-                <div className="header-contact-container">
-                    <h2>503-480-5155</h2>
-                    <h2>liamd.todd5@gmail.com</h2>
+                <div className="nav">
+                    <h2 className="nav-item">Projects</h2>
+                    <h2 className="nav-item">Experience</h2>
+                    <h2 className="nav-item">Contact</h2>
                 </div>
-            </div>
+            </header>
+            
+            <About />
 
-            <div className="portfolio-page-container">
-                <PortfolioPage />
-            </div>
+            <Projects />
 
-            <div className="projects-container">
-                <h1>Projects</h1>
-                <div className="row-container">
-                    <Projects />
-                </div>
-            </div>
-
-            <div className="resume-page-container">
-                <Resume />
-            </div>
+            <Resume />
         </>
     )
 }
