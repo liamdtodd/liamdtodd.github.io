1607c3bcbcf20e993e16d23db37179149045c336
liamdtodd <91576595+liamdtodd@users.noreply.github.com>
2024-05-31 17:27:02 -0700

    header/nav work

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,22 +1,18 @@
-import { useState } from "react"
-import { css } from "@emotion/react"
-
 import Resume from "./components/Resume";
 import Projects from "./components/Projects";
 import About from "./components/About";
 
 export default function Home() {
-
     return (
         <>
             <header className="header-container">
                 <h1>Liam Todd's Portfolio</h1>
 
-                <div className="nav">
+                <nav className="nav">
                     <h2 className="nav-item">Projects</h2>
                     <h2 className="nav-item">Experience</h2>
                     <h2 className="nav-item">Contact</h2>
-                </div>
+                </nav>
             </header>
             
             <About />
