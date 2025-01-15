e1b280c4ac08a35480226a94b420031a95ee19c5
liamdtodd <91576595+liamdtodd@users.noreply.github.com>
2025-01-14 20:15:16 -0800

    test branch

--- src/Home.jsx
+++ src/Home.jsx
@@ -11,39 +11,7 @@ export default function Home() {
 
     return (
         <>
-            <header className="header-container">
-                <h1>Liam Todd's Portfolio</h1>
-
-                <nav className="nav">
-                    <h2 className="nav-item" onClick={() => {
-                        if (projectsRef.current)
-                            projectsRef.current.scrollIntoView({ behavior: "smooth" })
-                    }}>Projects</h2>
-                    <h2 className="nav-item" onClick={() => {
-                        if (resumeRef.current)
-                            resumeRef.current.scrollIntoView({ behavior: "smooth" });      
-                    }}>Experience</h2>
-                    <h2 className="nav-item" onClick={() => contactRef.current.scrollIntoView({ behavior: "smooth" })}>Contact</h2>
-                </nav>
-            </header>
-            
-            <About />
-
-            <Projects ref={projectsRef} />
-
-            <Resume ref={resumeRef} />
-
-            <footer ref={contactRef} className="footer-container">
-                <div className="name-container">
-                    <h1>Liam Todd</h1>
-                    <p>Web Developer & Software Engineer</p>
-                </div>
-
-                <div className="contacts">
-                    <h3 className="contact-item">(503) 480-5155</h3>
-                    <h3 className="contact-item">liamd.todd5@gmail.com</h3>
-                </div>
-            </footer>
+           
         </>
     )
 }
